1c75a5f47a089c7ad0291ab04930cae394b39438
Philip Sada <sadaphillip@gmail.com>
2020-03-09 16:56:52 +0100

    added show all user page plus pagination

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class UsersTableSeeder extends Seeder
 {
@@ -16,12 +17,16 @@ class UsersTableSeeder extends Seeder
             'lastname' => 'Sada',
             'email' => 'sadaphillip@gmail.com',
             'password' => Hash::make('12345678'),
+            'created_at'=>Carbon::now(),
+            'updated_at'=>Carbon::now()
         ]);
         DB::table('users')->insert([
             'firstname' => 'Philipe',
             'lastname' => 'Sadaa',
             'email' => 'sadaphillipe@gmail.com',
             'password' => Hash::make('12345678'),
+            'created_at'=>Carbon::now(),
+            'updated_at'=>Carbon::now()
         ]);
     }
 }
--- resources/views/admin/users/all.blade.php
+++ resources/views/admin/users/all.blade.php
@@ -37,6 +37,7 @@
                                    <thead>
                                        <tr>
                                            <th scope="col">Id</th>
+                                           <th scope="col">Name</th>
                                            <th scope="col">Email</th>
                                            <th scope="col">Date Created</th>
                                            <th scope="col">Edit</th>
@@ -44,16 +45,20 @@
                                        </tr>
                                    </thead>
                                    <tbody>
+                                       @foreach($users as $user)
                                        <tr>
-                                           <th scope="row">1</th>
-                                           <td>Burgers.admin@email.com</td>
-                                           <td>2/2/2020</td>
-                                           <td><a href="/admin/users/1/edit"><i class="far fa-edit"></i></a></td>
-                                           <td><a href="/admin/users/1/delete" 
+                                           <th scope="row">{{$user->id}}</th>
+                                           <th>{{$user->firstname}} {{$user->lastname}}</th>
+                                           <td>{{$user->email}}</td>
+                                           <td>{{date('m/d/y', strtotime($user->updated_at))}}</td>
+                                           <td><a href="/admin/users/{{$user->id}}/edit"><i class="far fa-edit"></i></a></td>
+                                           <td><a href="/admin/users/{{$user->id}}/delete" 
                                            onclick="if(! confirm('Are you sure you want to delete this user?')){return false}"><i class="far fa-trash-alt"></i></a></td>
                                        </tr>
+                                       @endforeach
                                    </tbody>
                                </table>
+                               {{ $users->links() }}
                            </div>
                        </div>
                    </div>
