a58cf75baa58ae62997d3b35ab595dd18040f5d1
Philip Sada <sadaphillip@gmail.com>
2020-07-06 17:56:18 +0200

    heroku

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\SeoSetting;
 use App\SocialAccountsSetting;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Illuminate\View\View;
+
 
 /*
 |--------------------------------------------------------------------------
